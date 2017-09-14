258214ecda3fa64736a99873089d951e129a1017
Hannah <hannah_hay-smith@brown.edu>
2017-09-14 16:08:31 -0400

    added butterknife and picasso

--- app/src/main/java/io/intrepid/thirdpartylibraryexcercise/MainActivity.java
+++ app/src/main/java/io/intrepid/thirdpartylibraryexcercise/MainActivity.java
@@ -1,17 +1,29 @@
 package io.intrepid.thirdpartylibraryexcercise;
 
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements MainContract.View, DownloadCatImageTask.Callback {
+import com.squareup.picasso.Picasso;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity implements MainContract.View {
+
+    @BindView(R.id.image_view)
     ImageView imageView;
+
+    @BindView(R.id.new_cat_button)
     Button getNewCatButton;
 
     private MainPresenter presenter;
@@ -21,25 +33,18 @@ public class MainActivity extends AppCompatActivity implements MainContract.View
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
         presenter = new MainPresenter();
         presenter.bindView(this);
-
-        imageView = (ImageView) findViewById(R.id.image_view);
-        getNewCatButton = (Button) findViewById(R.id.new_cat_button);
-        getNewCatButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                onNewCatButtonClick();
-            }
-        });
     }
+
 
     @Override
     protected void onResume() {
         super.onResume();
-        presenter.bindView(this);
     }
 
+    @OnClick(R.id.new_cat_button)
     public void onNewCatButtonClick() {
         presenter.onNewCatButtonClick();
     }
@@ -62,16 +67,19 @@ public class MainActivity extends AppCompatActivity implements MainContract.View
 
     @Override
     public void loadImageFromUrl(String url) {
-        DownloadCatImageTask downloadCatImageTask = new DownloadCatImageTask();
-        downloadCatImageTask.setCallback(this);
-        downloadCatImageTask.execute(url);
+        Picasso.with(this).load(url).into(imageView, new com.squareup.picasso.Callback() {
+            @Override
+            public void onSuccess() {
+                dismissLoadingIndicator();
+            }
+
+            @Override
+            public void onError() {
+
+            }
+        });
     }
 
-    @Override
-    public void onImageResult(Bitmap bitmap) {
-        dismissLoadingIndicator();
-        imageView.setImageBitmap(bitmap);
-    }
 
     @Override
     protected void onStop() {
